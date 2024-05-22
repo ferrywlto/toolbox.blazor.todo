070c0e99174f968f7773386334663248d9366a90
Ferry To <ferrywlto@gmail.com>
2024-05-23 00:04:24 +0100

    feat: Make frontend able to inject environment variable from docker to nginx then to frontend

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,11 +1,50 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using frontend;
+using System.Net.Http.Json;
+using Toolbox.Blazor.Todo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+var http = app.Services.GetRequiredService<HttpClient>();
+try {
+
+    var config = await http.GetFromJsonAsync<AppConfiguration>("config.js");
+    Console.WriteLine($"backend url: {config!.BackendUrl}");
+    AppState.Configuration.BackendUrl = config!.BackendUrl;
+} catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+// AppState.Configuration.BackendUrl = config!.BackendUrl;
+ 
+await app.RunAsync();
+
+namespace Toolbox.Blazor.Todo 
+{
+
+    public static class AppState
+    {
+        public static AppConfiguration Configuration { get; set; } = new();
+    }
+
+    public class AppConfiguration
+    {
+        public string BackendUrl { get; set; } = string.Empty;
+    }
+    // [JsonSerializable(typeof(TodoItem[]))]
+    // internal partial class AppJsonSerializerContext : JsonSerializerContext
+    // {
+
+    // }
+}
+
+
+
