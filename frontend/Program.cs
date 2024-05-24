a935e199ac0fefc403266c5673d563ad3e3a0447
Ferry To <ferrywlto@gmail.com>
2024-05-24 23:44:01 +0100

    fix: Blazor calling backend with nginx reverse proxy

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Net.Http.Json;
 using Toolbox.Blazor.Todo.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,29 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 var app = builder.Build();
-
-var http = app.Services.GetRequiredService<HttpClient>();
-try {
-
-    var config = await http.GetFromJsonAsync<AppConfiguration>("config.js");
-    Console.WriteLine($"backend url: {config!.BackendUrl}");
-    AppState.Configuration.BackendUrl = config!.BackendUrl;
-} catch (Exception e)
-{
-    Console.WriteLine(e);
-    AppState.Configuration.BackendUrl = "http://localhost:5000";
-}
-
-// AppState.Configuration.BackendUrl = config!.BackendUrl;
  
 await app.RunAsync();
-
-public static class AppState
-{
-    public static AppConfiguration Configuration { get; set; } = new();
-}
-
-public class AppConfiguration
-{
-    public string BackendUrl { get; set; } = string.Empty;
-}
