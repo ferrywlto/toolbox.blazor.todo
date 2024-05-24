0c7d8eeda96149a01a962ef28fee5f79bd85c940
Ferry To <ferrywlto@gmail.com>
2024-05-25 00:03:50 +0100

    fix: Blazor can call backend with standalone docker run command

--- build-run-docker.sh
+++ build-run-docker.sh
@@ -1,4 +1,4 @@
 docker build . -t toolbox.blazor.todo.backend:local  --file backend/Dockerfile
-docker build . -t toolbox.blazor.todo.frontend:local  --file frontend/Dockerfile
-docker run -d -p 80:80 -e API_URL=http://localhost:5000 toolbox.blazor.todo.frontend:local
 docker run -d -p 5000:5000 toolbox.blazor.todo.backend:local
+docker build . -t toolbox.blazor.todo.frontend:local  --file frontend/Dockerfile
+docker run --rm -p 80:80 -e API_URL=http://host.docker.internal:5000/ toolbox.blazor.todo.frontend:local
