6641df982f5bbd2ca5c1bb9b4747fa3dddf1a03c
Ferry To <ferrywlto@gmail.com>
2024-05-28 23:16:50 +0100

    fix: Scoped dependency in Singleton resolved

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -7,8 +7,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var config = builder.Configuration.Build();
+builder.Services.AddSingleton<IConfiguration>(sp => config);
+builder.Services.AddHttpClient<BackendAPIGateway>((sp, client) => 
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => AppState.GetInstance());
+builder.Services.AddSingleton(sp => {
+    var backendAPIGateway = sp.GetRequiredService<BackendAPIGateway>();
+    return new TodoItemStore(backendAPIGateway);
+});
 
 var app = builder.Build();
 await app.RunAsync();
